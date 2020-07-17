0b827205652cf5bd336e4a198fc28a10fcda9acf
balusreekanth <2246934+balusreekanth@users.noreply.github.com>
2020-07-17 13:48:01 +0530

    Create cc_redirect.php

---
+++ cc_redirect.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by Sreekanth Balu
+ * Date: 16/07/12020
+ * Email: balusreekanth@gmail.com
+ * Time: 12:54 PM
+ */
+
+
+/**
+* Licence:
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//this will redirect teh script to start page
+header('Refresh: 5; URL=click2call.html'); ?>
